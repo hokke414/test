75d14bdab6ae1b22d09c2e22921b336f3ca24834
keromatsu26 <30004582-keromatsu26@users.noreply.replit.com>
2024-02-26 23:40:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+client.commands = new Collection();
+
+const http = require("http");
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Bot is online!");
+  })
+  .listen(3000);
+
+// commandsフォルダから、.jsで終わるファイルのみを取得
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // 取得した.jsファイル内の情報から、コマンドと名前をListenner-botに対して設定
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING]  ${filePath} のコマンドには、必要な "data" または "execute" プロパティがありません。`,
+    );
+  }
+}
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content == "manato") {
+    message.channel.send("fuck");
+  } else if (message.content == "a") {
+    message.channel.send("bcdefghijklmnopqrstuvwxyz");
+  }
+});
+
+// コマンドが送られてきた際の処理
+client.on(Events.InteractionCreate, async (interaction) => {
+  // コマンドでなかった場合は処理せずさよなら。
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  // 一致するコマンドがなかった場合
+  if (!command) {
+    console.error(` ${interaction.commandName} というコマンドは存在しません。`);
+    return;
+  }
+
+  try {
+    // コマンドを実行
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "コマンドを実行中にエラーが発生しました。",
+      ephemeral: true,
+    });
+  }
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+  //ステータス設定
+  client.user.setPresence({ activities: [{ name: `人生` }], status: "online" });
+});
+
+client.login(process.env.TOKEN);
